ee1a1c37fb0c61fbd4d97dbf4a0974250577952a
BeauX <fire-fly_7@hotmail.com>
2024-05-10 13:37:06 -0700

    ex 3 commit

--- app.js
+++ app.js
@@ -20,3 +20,14 @@ space below:
 foods.push('pizza', 'cheeseburger');
 
 console.log('Exercise 2 result:', foods);
+
+/*
+Exercise 3: Insert at the beginning
+1) Insert the string 'taco' at the beginning of the 
+`foods` array.
+Complete Exercise 3 in the space below:
+*/
+
+foods.unshift('taco');
+
+console.log('Exercise 2 result;', foods);
